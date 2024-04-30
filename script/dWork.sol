b1a1e3abeb4ef4c6b7904352b42f36dc71e6ffdd
makushi <mzozord@gmail.com>
2024-04-30 11:04:12 +0200

    Project init + OZ, CCIP and Chainlink lib + OracleLib.sol to pause on stale pricefeeds * Updated foundry.toml * Created dWork.sol

---
+++ script/dWork.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract dWork {}
+
+contract dWorkShare {}
