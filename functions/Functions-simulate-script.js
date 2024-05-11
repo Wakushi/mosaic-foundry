61079062622ed44ccdf16f1c6297dd38df71d7ee
makushi <mzozord@gmail.com>
2024-05-11 10:22:29 +0200

    Functions prep + chainlink/env-enc + chainlink/functions-toolkit Added CF config, simulation script and mock json data

---
+++ functions/Functions-simulate-script.js
@@ -0,0 +1,14 @@
+const { simulateScript } = require("@chainlink/functions-toolkit")
+const requestConfig = require("./Functions-request-config")
+
+async function main() {
+  const { responseBytesHexstring, capturedTerminalOutput, errorString } =
+    await simulateScript(requestConfig)
+
+  console.log("bytes response:", responseBytesHexstring)
+  console.log("error: ", errorString)
+  console.log("Output: ", capturedTerminalOutput)
+}
+
+// node Functions-simulate-script.js
+main()
