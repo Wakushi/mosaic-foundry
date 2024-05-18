801df35b897a99e26af0f8a791c0650661c5e868
makushi <mzozord@gmail.com>
2024-05-18 22:45:10 +0200

    Updated contract addresses and encrypted secrets

--- script/DeploydWorkFactory.s.sol
+++ script/DeploydWorkFactory.s.sol
@@ -7,8 +7,8 @@ import {dWorkFactory} from "../src/dWorkFactory.sol";
 import {IGetWorkVerifierReturnTypes} from "../src/interfaces/IGetWorkVerifierReturnTypes.sol";
 
 contract DeployDWorkFactory is Script {
-    address workVerifier = 0x486Bc902b120741Dff8563d0462af105F49598db;
-    address workSharesManager = 0x0bbc414f42DB6Ad35eEF14c553c49B63e368E965;
+    address workVerifier = 0xD69752D5F0fd86A2241c1bb5B2c1a1b0A486F155;
+    address workSharesManager = 0x60f7b9f6f83b38e98CCAB2b594F4bABd830307Ae;
 
     function run() external returns (dWorkFactory) {
         HelperConfig helperConfig = new HelperConfig();
