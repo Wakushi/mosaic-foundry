4a922f739b882461aa8ee68a909a14f32d30dbf3
makushi <mzozord@gmail.com>
2024-05-20 19:28:08 +0200

    Marketplace update - Added MarketShareItem struct - Updated dWorkSharesManager contract with listing, unlisting and buying share methods

--- src/dWorkSharesManager.sol
+++ src/dWorkSharesManager.sol
@@ -20,17 +20,31 @@ contract dWorkSharesManager is ERC1155, Ownable {
         bool isPaused;
     }
 
+    struct MarketShareItem {
+        uint256 itemId;
+        uint256 sharesTokenId;
+        uint256 priceUsd;
+        address seller;
+        bool isSold;
+    }
+
     ///////////////////
     // State variables
     ///////////////////
 
     // Chainlink Data Feed
     AggregatorV3Interface private s_priceFeed;
-    address s_dWork;
-    uint256 s_tokenId;
 
+    address s_dWork;
+    uint256 s_shareTokenId;
+    uint256 s_marketShareItemId;
+    uint256[] s_listedShareItemsIds;
     mapping(uint256 sharesTokenId => WorkShares workShares) s_workShares;
     mapping(uint256 workTokenId => uint256 sharesTokenId) s_sharesTokenIdByWorkId;
+    mapping(uint256 marketShareItemId => MarketShareItem marketShareItem) s_marketShareItems;
+    mapping(uint256 marketShareItemId => bool isListed) s_isItemListed;
+    mapping(uint256 listedShareItem => uint256 index)
+        public s_listedShareItemIndex;
 
     ///////////////////
     // Events
@@ -44,16 +58,34 @@ contract dWorkSharesManager is ERC1155, Ownable {
     );
     event ShareBought(uint256 sharesTokenId, uint256 amount, address buyer);
     event SharesPaused(WorkShares workShares, bool isPaused);
+    event MarketShareItemListed(
+        uint256 marketShareItemId,
+        uint256 sharesTokenId,
+        uint256 amount,
+        uint256 priceUsd,
+        address seller
+    );
+    event MarketShareItemSold(
+        uint256 marketShareItemId,
+        uint256 sharesTokenId,
+        uint256 amount,
+        uint256 priceUsd,
+        address buyer
+    );
+    event MarketShareItemUnlisted(uint256 marketShareItemId);
 
     //////////////////
     // Errors
     ///////////////////
 
-    error dWorkShare__InsufficientFunds();
-    error dWorkShare__InitialSaleClosed();
-    error dWork__NotWorkContract();
-    error dWork__SharesPaused();
-    error dWork__TokenIdDoesNotExist();
+    error dWorkSharesManager__InsufficientFunds();
+    error dWorkSharesManager__InitialSaleClosed();
+    error dWorkSharesManager__NotWorkContract();
+    error dWorkSharesManager__SharesPaused();
+    error dWorkSharesManager__TokenIdDoesNotExist();
+    error dWorkSharesManager__ItemNotListed();
+    error dWorkSharesManager__NotItemSeller();
+    error dWorkSharesManager__ItemAlreadySold();
 
     //////////////////
     // Modifiers
@@ -98,12 +130,12 @@ contract dWorkSharesManager is ERC1155, Ownable {
         _ensureOnlydWork();
 
         unchecked {
-            ++s_tokenId;
+            ++s_shareTokenId;
         }
 
-        _mint(_workOwner, s_tokenId, _shareSupply, "");
+        _mint(_workOwner, s_shareTokenId, _shareSupply, "");
 
-        s_workShares[s_tokenId] = WorkShares({
+        s_workShares[s_shareTokenId] = WorkShares({
             maxShareSupply: _shareSupply,
             sharePriceUsd: _sharePriceUsd,
             workTokenId: _workTokenId,
@@ -113,11 +145,16 @@ contract dWorkSharesManager is ERC1155, Ownable {
             isPaused: false
         });
 
-        s_sharesTokenIdByWorkId[_workTokenId] = s_tokenId;
+        s_sharesTokenIdByWorkId[_workTokenId] = s_shareTokenId;
 
-        emit SharesCreated(s_tokenId, _shareSupply, _workTokenId, _workOwner);
+        emit SharesCreated(
+            s_shareTokenId,
+            _shareSupply,
+            _workTokenId,
+            _workOwner
+        );
 
-        return s_tokenId;
+        return s_shareTokenId;
     }
 
     function buyInitialShare(
@@ -125,7 +162,7 @@ contract dWorkSharesManager is ERC1155, Ownable {
         uint256 _shareAmount
     ) external payable whenSharesNotPaused(_sharesTokenId) {
         if (_sharesTokenId == 0) {
-            revert dWork__TokenIdDoesNotExist();
+            revert dWorkSharesManager__TokenIdDoesNotExist();
         }
 
         WorkShares storage workShares = s_workShares[_sharesTokenId];
@@ -134,14 +171,14 @@ contract dWorkSharesManager is ERC1155, Ownable {
             workShares.totalShareBought + _shareAmount >
             workShares.maxShareSupply
         ) {
-            revert dWorkShare__InitialSaleClosed();
+            revert dWorkSharesManager__InitialSaleClosed();
         }
 
         uint256 sentValueUsd = msg.value.getConversionRate(s_priceFeed);
         uint256 usdAmountDue = _shareAmount * workShares.sharePriceUsd;
 
         if (sentValueUsd < usdAmountDue) {
-            revert dWorkShare__InsufficientFunds();
+            revert dWorkSharesManager__InsufficientFunds();
         }
 
         unchecked {
@@ -158,6 +195,111 @@ contract dWorkSharesManager is ERC1155, Ownable {
         );
 
         emit ShareBought(_sharesTokenId, _shareAmount, msg.sender);
+    }
+
+    function listMarketShareItem(
+        uint256 _sharesTokenId,
+        uint256 _amount,
+        uint256 _priceUsd
+    ) external whenSharesNotPaused(_sharesTokenId) {
+        if (_sharesTokenId == 0) {
+            revert dWorkSharesManager__TokenIdDoesNotExist();
+        }
+
+        safeTransferFrom(
+            msg.sender,
+            address(this),
+            _sharesTokenId,
+            _amount,
+            ""
+        );
+
+        unchecked {
+            ++s_marketShareItemId;
+        }
+
+        s_marketShareItems[s_marketShareItemId] = MarketShareItem({
+            itemId: s_marketShareItemId,
+            sharesTokenId: _sharesTokenId,
+            priceUsd: _priceUsd,
+            seller: msg.sender,
+            isSold: false
+        });
+
+        s_isItemListed[s_marketShareItemId] = true;
+
+        s_listedShareItemsIds.push(s_marketShareItemId);
+        s_listedShareItemIndex[s_marketShareItemId] =
+            s_listedShareItemsIds.length -
+            1;
+
+        emit MarketShareItemListed(
+            s_marketShareItemId,
+            _sharesTokenId,
+            _amount,
+            _priceUsd,
+            msg.sender
+        );
+    }
+
+    function buyMarketShareItem(uint256 _marketShareItemId) external payable {
+        if (!s_isItemListed[_marketShareItemId]) {
+            revert dWorkSharesManager__ItemNotListed();
+        }
+
+        MarketShareItem storage marketShareItem = s_marketShareItems[
+            _marketShareItemId
+        ];
+
+        if (marketShareItem.isSold) {
+            revert dWorkSharesManager__ItemAlreadySold();
+        }
+
+        uint256 sentValueUsd = msg.value.getConversionRate(s_priceFeed);
+
+        if (sentValueUsd < marketShareItem.priceUsd) {
+            revert dWorkSharesManager__InsufficientFunds();
+        }
+
+        _safeTransferFrom(
+            address(this),
+            msg.sender,
+            marketShareItem.sharesTokenId,
+            1,
+            ""
+        );
+
+        marketShareItem.isSold = true;
+
+        _unlistItem(_marketShareItemId);
+
+        emit MarketShareItemSold(
+            _marketShareItemId,
+            marketShareItem.sharesTokenId,
+            1,
+            marketShareItem.priceUsd,
+            msg.sender
+        );
+    }
+
+    function unlistMarketShareItem(uint256 _marketShareItemId) external {
+        if (!s_isItemListed[_marketShareItemId]) {
+            revert dWorkSharesManager__ItemNotListed();
+        }
+
+        MarketShareItem storage marketShareItem = s_marketShareItems[
+            _marketShareItemId
+        ];
+
+        if (marketShareItem.seller != msg.sender) {
+            revert dWorkSharesManager__NotItemSeller();
+        }
+
+        s_isItemListed[_marketShareItemId] = false;
+
+        _unlistItem(_marketShareItemId);
+
+        emit MarketShareItemUnlisted(_marketShareItemId);
     }
 
     function setDWorkAddress(address _dWork) external onlyOwner {
@@ -204,14 +346,14 @@ contract dWorkSharesManager is ERC1155, Ownable {
 
     function _ensureOnlydWork() internal view {
         if (msg.sender != s_dWork) {
-            revert dWork__NotWorkContract();
+            revert dWorkSharesManager__NotWorkContract();
         }
     }
 
     function _ensureSharesNotPaused(uint256 _sharesTokenId) internal view {
         WorkShares storage workShares = s_workShares[_sharesTokenId];
         if (workShares.isPaused) {
-            revert dWork__SharesPaused();
+            revert dWorkSharesManager__SharesPaused();
         }
     }
 
@@ -221,9 +363,22 @@ contract dWorkSharesManager is ERC1155, Ownable {
         for (uint256 i = 1; i < _sharesTokenIds.length; i++) {
             WorkShares storage workShares = s_workShares[_sharesTokenIds[i]];
             if (workShares.isPaused) {
-                revert dWork__SharesPaused();
+                revert dWorkSharesManager__SharesPaused();
             }
         }
+    }
+
+    function _unlistItem(uint256 _marketShareItemId) internal {
+        uint256 index = s_listedShareItemIndex[_marketShareItemId];
+        uint256 lastItemId = s_listedShareItemsIds[
+            s_listedShareItemsIds.length - 1
+        ];
+
+        s_listedShareItemsIds[index] = lastItemId;
+        s_listedShareItemIndex[lastItemId] = index;
+
+        s_listedShareItemsIds.pop();
+        delete s_listedShareItemIndex[_marketShareItemId];
     }
 
     ////////////////////
@@ -235,7 +390,7 @@ contract dWorkSharesManager is ERC1155, Ownable {
     }
 
     function getLastTokenId() external view returns (uint256) {
-        return s_tokenId;
+        return s_shareTokenId;
     }
 
     function getSharesTokenIdByWorkId(
@@ -253,5 +408,16 @@ contract dWorkSharesManager is ERC1155, Ownable {
     function getNativeTokenPriceUsd() external view returns (uint256) {
         (, int256 answer, , , ) = s_priceFeed.latestRoundData();
         return uint256(answer * 10000000000);
+    }
+
+    function getListedItems() external view returns (MarketShareItem[] memory) {
+        uint256 listedCount = s_listedShareItemsIds.length;
+        MarketShareItem[] memory items = new MarketShareItem[](listedCount);
+
+        for (uint256 i = 0; i < listedCount; i++) {
+            items[i] = s_marketShareItems[s_listedShareItemsIds[i]];
+        }
+
+        return items;
     }
 }
